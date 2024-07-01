b921475a61219a6c6be5f41651cefca5fd3b51ba
HHessoquat <146068799+HHessoquat@users.noreply.github.com>
2024-07-01 11:27:57 +0200

    Update le_petit_coin.sql
    add DROP to sql init sheet

--- le_petit_coin.sql
+++ le_petit_coin.sql
@@ -1,3 +1,12 @@
+--DROP TABLE RETRAITS;
+--DROP TABLE ENCHERES;
+--DROP TABLE ARTICLES_VENDUS;
+--DROP TABLE UTILISATEURS;
+--DROP TABLE CATEGORIES;
+
+--USE master;
+--DROP DATABASE ENIEncheres;
+
 CREATE DATABASE ENIEncheres;
 go
 USE ENIEncheres;
@@ -8,10 +17,11 @@ go
 
 
 
+
 CREATE TABLE CATEGORIES (
     no_categorie   INTEGER IDENTITY(1,1) NOT NULL,
     libelle        VARCHAR(30) NOT NULL
-)
+);
 
 ALTER TABLE CATEGORIES ADD constraint categorie_pk PRIMARY KEY (no_categorie)
 
@@ -21,7 +31,7 @@ CREATE TABLE ENCHERES (
     date_enchere     datetime NOT NULL,
 	montant_enchere  INTEGER NOT NULL
 
-)
+);
 
 ALTER TABLE ENCHERES ADD constraint enchere_pk PRIMARY KEY (no_utilisateur, no_article)
 
@@ -30,7 +40,7 @@ CREATE TABLE RETRAITS (
     rue              VARCHAR(30) NOT NULL,
     code_postal      VARCHAR(15) NOT NULL,
     ville            VARCHAR(30) NOT NULL
-)
+);
 
 ALTER TABLE RETRAITS ADD constraint retrait_pk PRIMARY KEY  (no_article)
 
@@ -47,9 +57,9 @@ CREATE TABLE UTILISATEURS (
     mot_de_passe     VARCHAR(30) NOT NULL,
     credit           INTEGER NOT NULL,
     administrateur   bit NOT NULL
-)
+);
 
-ALTER TABLE UTILISATEURS ADD constraint utilisateur_pk PRIMARY KEY (no_utilisateur)
+ALTER TABLE UTILISATEURS ADD constraint utilisateur_pk PRIMARY KEY (no_utilisateur);
 
 
 CREATE TABLE ARTICLES_VENDUS (
@@ -62,35 +72,36 @@ CREATE TABLE ARTICLES_VENDUS (
     prix_vente                    INTEGER,
     no_utilisateur                INTEGER NOT NULL,
     no_categorie                  INTEGER NOT NULL
-)
+);
 
-ALTER TABLE ARTICLES_VENDUS ADD constraint articles_vendus_pk PRIMARY KEY (no_article)
+ALTER TABLE ARTICLES_VENDUS ADD constraint articles_vendus_pk PRIMARY KEY (no_article);
 
 ALTER TABLE ARTICLES_VENDUS
     ADD CONSTRAINT encheres_utilisateur_fk FOREIGN KEY ( no_utilisateur ) REFERENCES UTILISATEURS ( no_utilisateur )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
 
 ALTER TABLE ENCHERES
     ADD CONSTRAINT encheres_articles_vendus_fk FOREIGN KEY ( no_article )
         REFERENCES ARTICLES_VENDUS ( no_article )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
 
 ALTER TABLE RETRAITS
     ADD CONSTRAINT retraits_articles_vendus_fk FOREIGN KEY ( no_article )
         REFERENCES ARTICLES_VENDUS ( no_article )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
 
 ALTER TABLE ARTICLES_VENDUS
     ADD CONSTRAINT articles_vendus_categories_fk FOREIGN KEY ( no_categorie )
         REFERENCES categories ( no_categorie )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
 
 ALTER TABLE ARTICLES_VENDUS
     ADD CONSTRAINT ventes_utilisateur_fk FOREIGN KEY ( no_utilisateur )
         REFERENCES utilisateurs ( no_utilisateur )
 ON DELETE NO ACTION 
-    ON UPDATE no action 
+    ON UPDATE no action ;
+
