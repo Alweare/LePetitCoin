4d4f445164111e3ee17c89936c5900f3e99f1307
tonioshakka <tonioshakka@gmail.com>
2024-07-01 15:01:21 +0200

    add html connexion et son controller
    ajout page html connexion avec son controller

---
+++ src/main/java/fr/eni/enchere/controllers/ConnexionController.java
@@ -0,0 +1,14 @@
+package fr.eni.enchere.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConnexionController {
+
+	 @GetMapping("/connexion")
+	    public String connexion() {
+	        return "connexion";
+	    }
+	
+}
