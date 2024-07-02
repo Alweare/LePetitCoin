f7992ce76492f2602e486332bb26ca8810b3320c
Yanis Renard-dumont <74371878+Alweare@users.noreply.github.com>
2024-07-02 12:13:45 +0200

    FIX nom des méthode et check
    Passage en français des méthode et des check

--- src/main/java/fr/eni/enchere/dal/EnchereDAOImpl.java
+++ src/main/java/fr/eni/enchere/dal/EnchereDAOImpl.java
@@ -1,5 +1,57 @@
 package fr.eni.enchere.dal;
 
+import java.util.List;
+
+import fr.eni.enchere.bo.Enchere;
+
 public class EnchereDAOImpl implements EnchereDAO {
+
+	@Override
+	public List<Enchere> trouveTout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Enchere> touveToutEnCours() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Enchere> trouveEnCoursParCategorie(int idCat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Enchere> trouveEnchereGagneeParUtilisateur(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enchere lire(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Enchere> trouverParVendeur(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Enchere> trouveParEncherisseur(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void creer(Enchere enchere) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
