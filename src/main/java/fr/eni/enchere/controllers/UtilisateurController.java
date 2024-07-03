1da0f8aa1fb53ddd71e54fb3612541b73a67ef13
Yanis Renard-dumont <74371878+Alweare@users.noreply.github.com>
2024-07-03 14:32:05 +0200

    ADD test frag errors

--- src/main/java/fr/eni/enchere/controllers/UtilisateurController.java
+++ src/main/java/fr/eni/enchere/controllers/UtilisateurController.java
@@ -2,6 +2,8 @@ package fr.eni.enchere.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -11,51 +13,51 @@ import fr.eni.enchere.exceptions.BusinessException;
 
 @Controller
 public class UtilisateurController {
-	
+
 	private UtilisateurServiceImpl utilisateurImpl;
 
-	
-	
+
+
 	public UtilisateurController(UtilisateurServiceImpl utilisateurImpl) {
 
 		this.utilisateurImpl = utilisateurImpl;
 	}
-	
+
 	@GetMapping("/creationProfil")
 	public String creationProfil(Model model) {
 		Utilisateur newUtilisateur = new Utilisateur();
 		model.addAttribute("newUtilisateur", newUtilisateur);
-		
+
 		return "creationProfil";
 	}
-	
-	@PostMapping("/creationProfil")
-	public String inscriptionUtilisateur (Utilisateur utilisateur)throws BusinessException {
-		BusinessException be = new BusinessException();
-		Utilisateur nouveauUtilisateur = new Utilisateur();
-		
-		if(utilisateur != null) {
-			this.utilisateurImpl.creerUtilisateur(utilisateur);
-			be.add("Veuillez saisir tout les champs");
-		}else {
-			throw be;
-		}
-		
-		
-		return "view-index";
-		
-	}
-	
-	
-	@GetMapping("/monProfil")
-	public String monProfil() {
-		return "monProfil";
-	}
-	
-	
-	
-	
-	
-	
 
-}
+	@PostMapping("/creationProfil")
+	public String inscriptionUtilisateur (Utilisateur utilisateur, BindingResult bindingResult)throws BusinessException {
+		if(bindingResult.hasErrors()) {
+			return "view-creationProfil";
+		}
+
+		try {
+			this.utilisateurImpl.creerUtilisateur(utilisateur);
+			return "redirect:/";
+		}
+		catch (BusinessException e) {
+			e.getErreurs().forEach(err -> {
+				ObjectError error = new ObjectError("globalError", err);
+				bindingResult.addError(error);
+
+			});
+			return "view-index";}
+
+		}
+
+
+		@GetMapping("/monProfil")
+		public String monProfil() {
+			return "monProfil";
+		}
+
+
+
+
+	}
