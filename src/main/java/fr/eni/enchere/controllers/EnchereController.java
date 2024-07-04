3f9bdc74274982f69ae3b52cf0351adbee0dff29
HHessoquat <146068799+HHessoquat@users.noreply.github.com>
2024-07-04 10:37:21 +0200

    fiw database v9

--- src/main/java/fr/eni/enchere/controllers/EnchereController.java
+++ src/main/java/fr/eni/enchere/controllers/EnchereController.java
@@ -32,7 +32,6 @@ public class EnchereController {
 	 @GetMapping("/")
 	 public String acceuil(Model model) {
 		 model.addAttribute("listeEncheres", articleService.recupererEnchereEnCours());
-		 System.out.println(model.getAttribute("listeEncheres"));
 		 return "view-index";
 	 }
 
