d51988def8b9931980416397a9f7c6205a401ea4
HHessoquat <146068799+HHessoquat@users.noreply.github.com>
2024-07-05 14:45:24 +0200

    feat StringToCaategory converter

---
+++ src/main/java/fr/eni/enchere/controllers/converters/ChaineVersCategorieConverter.java
@@ -0,0 +1,12 @@
+package fr.eni.enchere.controllers.converters;
+
+import org.springframework.core.convert.converter.ConverterFactory;
+
+import fr.eni.enchere.bo.Categorie;
+
+public class ChaineVersCategorieConverter implements ConverterFactory<String,Categorie> {
+
+
+	}
+
+}
